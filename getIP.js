fa0751090dac35ff2c5f0f407ecbc5912f81da1b
bigfishgo <58214240+bigfishgo@users.noreply.github.com>
2021-03-23 16:03:31 +0800

    Create getIP.js

---
+++ getIP.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const https = require('https')
+var a
+function getStr() {
+    return new Promise((resolve, reject) => {
+        http.get("http://ip.json-json.com", data => {
+            var str = "";
+                data.on("data", chunk => {
+                    str += chunk
+                })
+            data.on("end", () => {
+                resolve(str)
+            })
+        })
+    })
+}
+
+setInterval(() => {
+    getStr().then(res => {
+        // console.log(res)
+        if(a!==res){
+            console.log(a);
+            console.log('-==========================-');
+            https.get(`https://&text=${res}`)
+        }
+        a = res
+ console.log(a);
+    })
+}, 3600);
