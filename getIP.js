d5c5c5463f148246156bdf2cc1a44a6695bef0cf
bigfishgo <haha@email.wang>
2021-08-22 23:49:02 +0800

    read URL from urlFile.txt

--- getIP.js
+++ getIP.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const https = require('https')
-var a
+const fs = require("fs");
+var ip
+var url
 function getStr() {
     return new Promise((resolve, reject) => {
         http.get("http://ip.json-json.com", data => {
@@ -15,15 +17,26 @@ function getStr() {
     })
 }
 
+fs.readFile("urlFile.txt", "utf-8", function(error, data) {
+    // console.log(error);  //如果err为null就说明读取成功了,没有出错
+    // console.log(data); // 如果不给第二个参数[读取的文件编码格式]就会以beffer格式输出
+
+    //  用error来判断文件是否读取成功
+    if (error) return console.log("读取文件失败,内容是" + error.message);
+    url = data;
+    console.log("读取文件成功,内容是" + data);
+
+});
+
 setInterval(() => {
     getStr().then(res => {
         // console.log(res)
-        if(a!==res){
-            console.log(a);
+        if(ip!==res){
+            console.log(ip);
             console.log('-==========================-');
-            https.get(`https://&text=${res}`)
+            https.get(`${url}&text=${res}`)
         }
-        a = res
- console.log(a);
+        ip = res
+ console.log(ip);
     })
-}, 3600000);
+}, 1000);
